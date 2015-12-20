f6083201c48c793053cdd592b173dbc97e6dad26
Costas Basdekis <costas.basdekis@gmail.com>
2015-12-20 22:11:44 +0000

    Break TestCase into factory and related TestCases

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from unittest import TestCase, main as unittest_main
 import main
 
 
-class TestStations(TestCase):
+class Factory(object):
 	STATION_1_ID_STR, STATION_1_NAME = "1", "London Bridge"
 	STATION_1_ID = int(STATION_1_ID_STR)
 
@@ -23,53 +23,58 @@ class TestStations(TestCase):
 		[STATION_1_ID_STR, STATION_2_ID_STR],
 	]
 
-	def create_stations(self):
+	@classmethod
+	def create_stations(cls):
 		return main.Stations()
 
-	def create_stations_with_json_stations(self):
-		stations = self.create_stations()
-		stations.load_stations_from_json(self.STATIONS_JSON)
+	@classmethod
+	def create_stations_with_json_stations(cls):
+		stations = cls.create_stations()
+		stations.load_stations_from_json(cls.STATIONS_JSON)
 
 		return stations
 
-	def create_stations_with_json_stations_and_connections(self):
-		stations = self.create_stations_with_json_stations()
-		stations.load_connections_from_json(self.CONNECTIONS_JSON)
+	@classmethod
+	def create_stations_with_json_stations_and_connections(cls):
+		stations = cls.create_stations_with_json_stations()
+		stations.load_connections_from_json(cls.CONNECTIONS_JSON)
 
 		return stations
 
+
+class TestStations(TestCase):
 	def test_create_stations(self):
-		self.create_stations()
+		Factory.create_stations()
 
 	def test_loading_stations(self):
-		self.create_stations_with_json_stations()
+		Factory.create_stations_with_json_stations()
 
 	def test_station_by_id_name_matches_from_loaded_stations(self):
-		stations = self.create_stations_with_json_stations()
+		stations = Factory.create_stations_with_json_stations()
 
-		self.assertIn(self.STATION_1_ID, stations.stations_by_id)
-		station_1 = stations.by_id(self.STATION_1_ID)
-		self.assertEquals(station_1._id, self.STATION_1_ID)
-		self.assertEquals(station_1.name, self.STATION_1_NAME)
+		self.assertIn(Factory.STATION_1_ID, stations.stations_by_id)
+		station_1 = stations.by_id(Factory.STATION_1_ID)
+		self.assertEquals(station_1._id, Factory.STATION_1_ID)
+		self.assertEquals(station_1.name, Factory.STATION_1_NAME)
 
 	def test_loading_connections(self):
-		stations = self.create_stations_with_json_stations_and_connections()
+		stations = Factory.create_stations_with_json_stations_and_connections()
 
 	def test_stations_are_connected(self):
-		stations = self.create_stations_with_json_stations_and_connections()
+		stations = Factory.create_stations_with_json_stations_and_connections()
 
-		station_1 = stations.by_id(self.STATION_1_ID)
-		station_2 = stations.by_id(self.STATION_2_ID)
+		station_1 = stations.by_id(Factory.STATION_1_ID)
+		station_2 = stations.by_id(Factory.STATION_2_ID)
 
 		self.assertIn(station_2, station_1.connections)
 		self.assertIn(station_1, station_2.connections)
 
 	def test_stations_are_not_connected(self):
-		stations = self.create_stations_with_json_stations_and_connections()
+		stations = Factory.create_stations_with_json_stations_and_connections()
 
-		station_1 = stations.by_id(self.STATION_1_ID)
-		station_2 = stations.by_id(self.STATION_2_ID)
-		station_3 = stations.by_id(self.STATION_3_ID)
+		station_1 = stations.by_id(Factory.STATION_1_ID)
+		station_2 = stations.by_id(Factory.STATION_2_ID)
+		station_3 = stations.by_id(Factory.STATION_3_ID)
 
 		self.assertNotIn(station_3, station_1.connections)
 		self.assertNotIn(station_1, station_3.connections)
