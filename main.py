41b39534ffe8c9c12f19836ad0ad9802f758c65a
Costas Basdekis <costas.basdekis@gmail.com>
2015-12-20 15:09:10 +0000

    Load connections from JSON file

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Stations(object):
 
 	def load_from_json_files(self, stations_filename, connections_filename):
 		self.load_stations_from_json_file(stations_filename)
+		self.load_connections_from_json_file(connections_filename)
 
 	def load_stations_from_json_file(self, stations_filename):
 		with open(stations_filename, 'rb') as f:
@@ -23,6 +24,16 @@ class Stations(object):
 		for _id, name in raw_stations:
 			self.create_station(_id, name)
 
+	def load_connections_from_json_file(self, connections_filename):
+		with open(connections_filename, 'rb') as f:
+			raw_connections = json.load(f)
+
+		for first_id, second_id in raw_connections:
+			first_station = self.stations_by_id[first_id]
+			second_station = self.stations_by_id[second_id]
+
+			first_station.connect_with(second_station)
+
 
 class Station(object):
 	def __init__(self, _id, name, stations):
@@ -30,12 +41,20 @@ class Station(object):
 		self.name = name
 		self.stations = stations
 		self.stations.add_station(self)
+		self.connections_by_id = {}
+
+	def connect_with(self, station):
+		if station._id in self.connections_by_id:
+			return
+
+		self.connections_by_id[station._id] = station
+		station.connect_with(self)
 
 
 def main():
 	stations = Stations()
-	stations.load_from_json_files("./tfl_stations.json", "")
-	print 'Loaded', len(stations.stations_by_name), "stations"
+	stations.load_from_json_files("./tfl_stations.json", "./tfl_connections.json")
+	print 'Loaded', len(stations.stations_by_name), "stations, with ", sum(len(station.connections_by_id) for station in stations.stations_by_name.itervalues()), "total connections"
 
 if __name__ == '__main__':
 	main()
